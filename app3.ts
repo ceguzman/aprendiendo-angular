31dd9370775c7be191923a016d8924600f7a36f8
Carlos esteban guzman baquero <ceguzman582@misena.edu.co>
2020-01-28 18:25:12 -0500

    funciones con parametros

---
+++ app3.ts
@@ -0,0 +1,14 @@
+/* 
+    Parametros en funciones 
+*/
+
+function saludar(nombre:string){
+     console.log("hola , " + nombre);
+}
+saludar("Carlos");
+
+
+function sumar(a:number , b:number){
+    console.log(a+b);
+}
+sumar(1,3);
