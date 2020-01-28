c38ea4af2f71744e137bcb02efae8895867e2b27
Carlos esteban guzman baquero <ceguzman582@misena.edu.co>
2020-01-28 17:54:24 -0500

    tipos de datos en typescript

---
+++ app.ts
@@ -0,0 +1,9 @@
+let nombre:string = "carlos";
+let apellido:string = "guzman";
+let edad:number = 18;
+
+let texto = `Hola, ${nombre} ${apellido} ${edad}`;
+console.log(texto);
+
+
+// variables var let const 
